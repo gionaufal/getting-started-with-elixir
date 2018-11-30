8c569122fce98f8dad1d1a7eedbd161cafad5a8c
Giovanni Naufal <gionaufal@gmail.com>
2018-11-30 21:54:15 -0200

    Private functions

--- sample_enum.exs
+++ sample_enum.exs
@@ -4,6 +4,11 @@ defmodule Sample.Enum do
   def first([], val), do: val
 
   def add(list, val \\ 0) do
+    trace(val)
     [val | list]
+  end
+
+  defp trace(string) do
+    IO.puts("The value passed in was #{string}")
   end
 end
