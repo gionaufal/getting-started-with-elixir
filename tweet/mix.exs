0f129b6d427f01be5513c90ae08ef96c463c6cee
Giovanni Naufal <gionaufal@gmail.com>
2018-11-30 23:19:57 -0200

    Create project and FileReader module

---
+++ tweet/mix.exs
@@ -0,0 +1,28 @@
+defmodule Tweet.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :tweet,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Tweet.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
