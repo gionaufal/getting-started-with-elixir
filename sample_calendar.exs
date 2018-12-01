22873e9b28ff6fcdd389af701051e4b4b255107e
Giovanni Naufal <gionaufal@gmail.com>
2018-11-30 22:14:01 -0200

    Cond

--- sample_calendar.exs
+++ sample_calendar.exs
@@ -3,6 +3,19 @@ defmodule Sample.Calendar do
   def leap_year(year) when rem(year, 400) == 0, do: true
   def leap_year(year) when rem(year, 100) == 0, do: false
   def leap_year(year) when rem(year, 4) == 0, do: true
-  def leap_year(year), do: false
+  def leap_year(_), do: false
+
+  def day_abbreviation(day) do
+    cond do
+      day == :Monday -> "M"
+      day == :Tuesday -> "Tu"
+      day == :Wednesday -> "W"
+      day == :Thrusday -> "Th"
+      day == :Friday -> "F"
+      day == :Saturday -> "Sa"
+      day == :Sunday -> "Su"
+      true -> "Invalid day"
+    end
+  end
 
 end
